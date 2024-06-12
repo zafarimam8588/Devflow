d1c6cbe160178affa9befc4cfe60250a536b897b
Zafar Imam <zimam8588@gmail.com>
2024-06-13 04:57:58 +0530

    fixed type in middleware.js

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  ignoredRoutes: ["/api/webhooks/cleark(.*)"],
+  ignoredRoutes: ["/api/webhooks/clerk(.*)"],
 });
 
 export const config = {
