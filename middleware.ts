6de016c373d3ce59ea1c4df4b1b1e773e346018a
Zafar Imam <zimam8588@gmail.com>
2024-06-13 04:56:42 +0530

    modified middleware file

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/api/webhooks",
-    "question/:id",
-    "/tags",
-    "/tags/:id",
-    "/profile/:id",
-    "/community",
-    "/jobs",
-  ],
-  ignoredRoutes: ["/api/webhooks(.*)"],
+  ignoredRoutes: ["/api/webhooks/cleark(.*)"],
 });
 
 export const config = {
