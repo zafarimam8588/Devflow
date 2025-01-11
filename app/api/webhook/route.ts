1654d1f7f3cbbf8657db812fe1723eef78e22254
Zafar Imam <zimam8588@gmail.com>
2025-01-11 17:23:09 +0530

    fixed bug in webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -92,9 +92,7 @@ export async function POST(req: Request) {
 
   if (eventType === "user.deleted") {
     const { id } = evt.data;
-    SkeletonAndToast
     console.log("Inside delete ");
-
 
     const deletedUser = await deleteUser({ clerkId: id! });
 
