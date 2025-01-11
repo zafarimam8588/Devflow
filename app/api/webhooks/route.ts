8ab5cc987f69ced170591feb43b8143c02cce557
Zafar Imam <zimam8588@gmail.com>
2025-01-11 19:50:27 +0530

    fixed bug in middleware

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -10,7 +10,6 @@ export async function POST(req: Request) {
   // TODO: Add your webhook secret to .env.local
   const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
   console.log("Inside routes webhook ");
-  console.log(WEBHOOK_SECRET + "..........................................");
   if (!WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
@@ -32,6 +31,7 @@ export async function POST(req: Request) {
 
   // Get the body
   const payload = await req.json();
+  console.log("Payload: ", payload);
   const body = JSON.stringify(payload);
 
   // Create a new SVIX instance with your secret.
--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,7 @@ export default authMiddleware({
     "/community",
     "/jobs",
   ],
-  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
+  ignoredRoutes: ["/api/webhooks", "/api/chatgpt"],
 });
 
 export const config = {
