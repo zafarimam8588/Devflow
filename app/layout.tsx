dff13c0be7868462c06705dd0f3d8b4aee8bfeef
Zafar Imam <zimam8588@gmail.com>
2025-01-11 17:31:33 +0530

    fixed bug in app/loyout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import "../styles/prism.css";
-import "./globals.css";
 
 import { Inter, Space_Grotesk } from "next/font/google";
 import type { Metadata } from "next";
